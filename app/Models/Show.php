477421c8fc001d4e5cf34903adbd0b0ae2403e8a
kourosh <the1kouroshkhalili@gmail.com>
2024-10-05 11:29:14 +0330

    [ADD] show migration , show model , showResource resource .

--- app/Models/Show.php
+++ app/Models/Show.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Show extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
+    public function podcasts()
+    {
+        return $this->hasMany(Podcast::class);
+    }
 }
--- database/migrations/2024_10_04_160459_create_shows_table.php
+++ database/migrations/2024_10_04_160459_create_shows_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('shows', function (Blueprint $table) {
             $table->id();
+            $table->string('title' , 256);
+            $table->text('cover');
+            $table->text('about');
             $table->timestamps();
         });
     }
