3c9c0c73179bf76f617703938de86757cd454366
Babak Chalaki <168085103+Babak-Chalacki@users.noreply.github.com>
2024-10-14 18:37:40 +0330

    [ADD] Like migration , Like model , LikesResource resource

--- database/migrations/2024_10_04_160355_create_likes_table.php
+++ database/migrations/2024_10_04_160355_create_likes_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('likes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('music_id')->constrained();
+            $table->foreignId('album_id')->constrained();
             $table->timestamps();
         });
     }
