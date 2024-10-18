5e0103b71cc6040f5ff12b9ccccd8ea2b9c3795b
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2024-10-18 18:23:33 +0330

    Merge branch 'main' of https://github.com/laravel-php-course/Spotify-API

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,44 +5,42 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterUserRequest;
 use Illuminate\Http\Request;
-use App\Models\User;
-use Illuminate\Support\Facades\Hash;
+use App\Repositories\UserRepository;
 use App\Trait\ApiResponse;
 
 class AuthController extends Controller
 {
     use ApiResponse;
 
+    public function __construct(private UserRepository $userRepository)
+    {
+        $this->repository = $userRepository;
+    }
+
     public function register(RegisterUserRequest $request)
     {
         try {
-            $user = User::create([
-                'email' => $request->email,
-                'mobile' => $request->mobile,
-                'username' => $request->username,
-                'password' => Hash::make($request->password),
-                'two_step_verificaztion' => false,
-                'subscribtion_plan' => 'FREE',
-                'role' => 'USER',
-            ]);
+            $user = $this->userRepository->create($request->all());
 
-            return $this->success('User registered successfully.', ['user' => $user]);
+            return $this->success('Welcome to our app. You are registered', [
+                'token' => $user->createToken('user todo token', [], now()->addWeek())->plainTextToken,
+                'user'  => $user
+            ]);
         } catch (\Exception $exception) {
-            return $this->error('User registration failed.', 500, [$exception->getMessage()]);
+            return $this->error($exception->getMessage(), 500);
         }
     }
+
     public function logout(Request $request)
     {
         try {
-            // Check if the user is authenticated
             if (!$request->user()) {
                 return $this->error('User not authenticated.', 401);
             }
 
-            // Delete the current access token
             $request->user()->currentAccessToken()->delete();
 
-            return $this->success('Logout successful',[]);
+            return $this->success('Logout successful', []);
         } catch (\Exception $exception) {
             return $this->error('Logout failed: ' . $exception->getMessage(), 500);
         }
