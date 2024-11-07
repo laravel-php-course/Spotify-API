658f1814969cc902daf09c3c3a8856fd833c8e17
kourosh <somthing.ir@gmail.com>
2024-11-07 11:18:50 +0330

    [ADD] Localized various methods in the Handler

--- lang/en/http_error_messages.php
+++ lang/en/http_error_messages.php
@@ -1,4 +1,9 @@
 <?php
 return [
-    'form_validation_error' => "Validation error."
+    'form_validation_error' => "Validation error." ,
+    'form_model_not_Found'  => 'source not found',
+    'form_authorization'    => "You are not authorized to perform this action",
+    'form_authentication'   => "Authentication failed" ,
+    'form_http'             => "HTTP error occurred" ,
+    'form_General'          => "An error occurred. Please try again later"
 ];
--- lang/fa/http_error_messages.php
+++ lang/fa/http_error_messages.php
@@ -1,4 +1,9 @@
 <?php
 return [
-    'form_validation_error' => "اعتبار سنجی با خطا رخ داد."
+    'form_validation_error' => "خطای اعتبارسنجی." ,
+    'form_model_not_Found'  => 'منبع یافت نشد.',
+    'form_authorization'    => "شما مجاز به انجام این عملیات نیستید.",
+    'form_authentication'   => "احراز هویت ناموفق بود." ,
+    'form_http'             => "خطای HTTP رخ داد." ,
+    'form_General'          => "خطایی رخ داد. لطفاً بعداً دوباره تلاش کنید."
 ];
