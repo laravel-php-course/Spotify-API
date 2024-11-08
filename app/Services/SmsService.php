be5ceee3cbf3269d0fd670b6f27fe11785e17d17
Babak-Chalacki <babak.chalaki82@gmail.com>
2024-11-08 17:09:12 +0330

    [FIX]UserRpository , SmsService

--- app/Services/SmsService.php
+++ app/Services/SmsService.php
@@ -1,7 +1,7 @@
 <?php
-
 namespace App\Services;
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
@@ -9,36 +9,24 @@ class SmsService
 {
     public static function generateOtpCode()
     {
-        return random_int(1000, 9999);
-    }
-
-    public static function convertToIranFormat($mobileNumber)
-    {
-        $mobileNumber = preg_replace('/\D/', '', $mobileNumber);
-        if (substr($mobileNumber, 0, 2) === '09') {
-            return '98' . substr($mobileNumber, 1);
-        } elseif (substr($mobileNumber, 0, 3) === '989') {
-            return $mobileNumber;
-        } elseif (substr($mobileNumber, 0, 4) === '+989') {
-            return substr($mobileNumber, 1);
-        } else {
-            return false; // فرمت نامعتبر
-        }
+        return rand(10000, 99999);
     }
 
     public static function sendOtp(string $phoneNumber, int $code)
     {
+        Cache::put('otp_' . $phoneNumber, $code, 300);
+
         $template = config('services.sms.template');
         $message = str_replace('{CODE}', $code, $template);
 
         try {
-        $response = Http::withoutVerifying()->withHeaders(['Content-Type' => 'application/x-www-form-urlencoded'])
-         ->post(config('services.sms.api'), [
-                'username' => config('services.sms.username'),
-                'password' => config('services.sms.password'),
-                'message' => $message,
-                'destination' => self::convertToIranFormat($phoneNumber),
-            ]);
+            $response = Http::withoutVerifying()->withHeaders(['Content-Type' => 'application/x-www-form-urlencoded'])
+                ->post(config('services.sms.api'), [
+                    'username' => config('services.sms.username'),
+                    'password' => config('services.sms.password'),
+                    'message' => $message,
+                    'destination' => self::convertToIranFormat($phoneNumber),
+                ]);
 
             if ($response->successful()) {
                 return true;
@@ -50,5 +38,11 @@ class SmsService
             Log::error('استثنای ارسال SMS', ['message' => $e->getMessage()]);
             return false;
         }
+    }
+
+    public static function convertToIranFormat($phoneNumber)
+    {
+
+        return $phoneNumber;
     }
 }
